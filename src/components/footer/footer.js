733e2ca14bb03ea701b7ae5b606bd16aae58d012
Clara-Manzano <clara.manzano.corona@gmail.com>
2025-01-02 13:15:52 +0100

    fixed LANDING

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,15 +4,10 @@ export const createFooter = () => {
   const footer = document.createElement("footer");
 
   const footerText = document.createElement("p");
-  footerText.textContent = "© 2025 Creado por Clara Manzano Corona | Código abierto en GitHub | Eclipso |";
-
-  const footerLink = document.createElement("a");
-  footerLink.href = "https://github.com/ClaraDevelope";
-  footerLink.textContent = "GitHub";
-  footerLink.target = "_blank";
+  footerText.innerHTML = `© 2025 Creado por Clara Manzano Corona | Código abierto en <a href="https://github.com/ClaraDevelope" target="_blank"> GitHub</a> | Eclipso`;
 
   footer.appendChild(footerText);
-  footer.appendChild(footerLink);
 
   app.append(footer);
 };
+
